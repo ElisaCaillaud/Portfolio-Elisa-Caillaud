e85d8abad70157adc2737ef324fcc356d3f55476
elisa <caillaud.elisa86@gmail.com>
2024-07-10 14:41:32 +0200

    installation Tailwind et mise en place du nav

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,18 @@
+function Header() {
+  return (
+    <div className="nav-full">
+      <nav>
+        <ul>
+          <li>
+            <a href="#accueil">Accueil</a>
+          </li>
+          <li>
+            <a href="#a-propos">À Propos</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      darkGreen: "#3C6255",
+      lightGreen: "#EDF1D6",
+      green: "#609966",
+      middleGreen: "#9DC08B",
+    },
+    fontFamily: {
+      abril: ["Abril", "serif"],
+      lato: ["Lato", "sans-serif"],
+    },
+    extend: {
+      fontWeight: {
+        normal: 400,
+        medium: 500,
+        bold: 700,
+      },
+    },
+  },
+
+  plugins: [],
+};
