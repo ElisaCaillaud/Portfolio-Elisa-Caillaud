8263f1343e608ba9c98c331160edc414e3ee12be
elisa <caillaud.elisa86@gmail.com>
2024-08-08 11:52:39 +0200

    section realisation grid et responsive

--- src/components/SectionRealisations/index.jsx
+++ src/components/SectionRealisations/index.jsx
@@ -7,16 +7,20 @@ const SectionRealisations = ({ indicesOutils }) => {
   const flatSkills = competences.flatMap((section) => section.list);
 
   return (
-    <div id="realisations" className="text-center mt-60 h-screen">
+    <div id="realisations" className="text-center mt-20 sm:mt-60">
       <h1 className="text-45px font-abril tracking-tight text-darkGreen sm:text-6xl">
         Réalisations
       </h1>
-      <div className="p-2 flex justify-center gap-6">
-        {indicesOutils.map((index) => {
-          return <Button key={index} type="fonce" text={flatSkills[index]} />;
-        })}
+      <div className="w-full flex justify-center">
+        <div className="p-2 flex flex-wrap w-4/5 justify-center gap-4 sm:gap-6">
+          {indicesOutils.map((index) => {
+            return <Button key={index} type="fonce" text={flatSkills[index]} />;
+          })}
+        </div>
       </div>
-      <BlocRealisations />
+      <div className="w-full flex justify-center">
+        <BlocRealisations />
+      </div>
     </div>
   );
 };
--- src/components/blocRealisations/index.jsx
+++ src/components/blocRealisations/index.jsx
@@ -1,26 +1,58 @@
 //Bloc de realisations principal affichant les infos principales
-import React from "react";
+import React, { useState, useEffect } from "react";
 import realisations from "../../datas/realisations.json";
+import Button from "../Competences";
+import "../../styles/index.css";
 
 const BlocRealisations = () => {
+  const [isSmallScreen, SmallScreen] = useState(window.innerWidth <= 768); //Initialisation de la largeur de l'ecran
+
+  useEffect(() => {
+    const resize = () => {
+      SmallScreen(window.innerWidth <= 768); // Fonction pour mettre à jour l'état chaque fois que la taille change
+    };
+
+    // Ajout d'un écouteur d'événement pour les changements de taille de l'écran
+    window.addEventListener("resize", resize);
+  }, []);
+
   return (
-    <div className="w-4/5 2xl:w-1/6 md:w-4/5 mt-6">
-      {realisations.map((realisation, index) => (
-        <div
-          key={index}
-          className={`p-4 rounded-md mb-4 w-full shadow-menu text-left`}
-          style={{
-            backgroundImage: `url(${realisation.cover})`,
-          }}
-        >
-          <h2 className="font-abril text-lightGreen text-40px">
-            {realisation.title}
-          </h2>
-          <p className="text-xl text-lightGreen font-medium">
-            {realisation.resume}
-          </p>
-        </div>
-      ))}
+    <div
+      className={`w-4/5 2xl:w-3/6 md:w-4/5 mt-6 grid-container ${
+        isSmallScreen ? "single-column" : "" //vérification de la taille de l'écran pour appliquer une ou plusieurs colonnes
+      }`}
+    >
+      {realisations.map((realisation, index) => {
+        //permet de déterminer si la ligne est pair ou impair, si 0=pair, si 1=impair
+        //vérifie si la ligne entiere est pair ou impair
+        const even = Math.floor(index / 2) % 2 === 0;
+
+        //vérifie si l'element est le premier de la pair par rapport á l'index
+        const pair = index % 2 === 0;
+
+        const gridStyle = {
+          gridColumnStart: even ? (pair ? 1 : 8) : pair ? 1 : 6, //debut de la colonne
+          gridColumnEnd: even ? (pair ? 8 : 13) : pair ? 6 : 13, //fin de la colonne
+        };
+
+        return (
+          <div key={index} style={gridStyle} className="card-container">
+            <div className="card flex flex-col items-center text-center justify-center h-80 p-4 rounded-md w-full shadow-menu bg-darkGreen">
+              <h2 className=" font-abril text-lightGreen text-40px">
+                {realisation.title}
+              </h2>
+              <p className="text-xl text-lightGreen font-medium">
+                {realisation.resume}
+              </p>
+              <Button
+                type="clair"
+                text="Découvrir"
+                style={{ marginTop: "5rem" }}
+              />{" "}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
