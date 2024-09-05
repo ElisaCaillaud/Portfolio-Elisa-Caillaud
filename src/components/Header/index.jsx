f9863cea764d5558f480f0ac9dad05a89bc6d4b1
elisa <caillaud.elisa86@gmail.com>
2024-09-05 10:53:12 +0200

    modification z-index menu burger

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -60,7 +60,7 @@ export default function Example() {
         className="lg:hidden"
       >
         <div className="fixed inset-0 z-10 bg-lightGreen" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <DialogPanel className="fixed inset-y-0 right-0 z-20 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <button
               type="button"
